0d76c0c0a49aa95b9efa050cb19d9706573ba892
HStakhiv <gsstahiv@gmail.com>
2022-11-10 17:54:27 +0200

    Make all pages login required

--- newsapp/views.py
+++ newsapp/views.py
@@ -1,44 +1,51 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.views import generic
 
 from newsapp.models import Redactor, Topic, Newspaper
 
 
+@login_required
 def index(request):
     num_redactors = Redactor.objects.count()
     num_topics = Topic.objects.count()
     num_newspapers = Newspaper.objects.count()
 
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
     context = {
         "num_redactors": num_redactors,
         "num_topics": num_topics,
         "num_newspapers": num_newspapers,
+        "num_visits": num_visits + 1,
     }
 
     return render(request, "newsapp/index.html", context=context)
 
 
-class TopicListView(generic.ListView):
+class TopicListView(LoginRequiredMixin, generic.ListView):
     model = Topic
     queryset = Topic.objects.order_by("name")
     paginate_by = 5
 
 
-class NewspaperListView(generic.ListView):
+class NewspaperListView(LoginRequiredMixin, generic.ListView):
     model = Newspaper
     paginate_by = 5
     queryset = Newspaper.objects.select_related("topic")
 
 
-class NewspaperDetailView(generic.DetailView):
+class NewspaperDetailView(LoginRequiredMixin, generic.DetailView):
     model = Newspaper
 
 
-class RedactorListView(generic.ListView):
+class RedactorListView(LoginRequiredMixin, generic.ListView):
     model = Redactor
     paginate_by = 5
 
 
-class RedactorDetailView(generic.DetailView):
+class RedactorDetailView(LoginRequiredMixin, generic.DetailView):
     model = Redactor
     queryset = Redactor.objects.prefetch_related("newspaper__topic")
