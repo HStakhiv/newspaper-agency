db406876a16d3873cbf475e78c44ec944b668db0
HStakhiv <gsstahiv@gmail.com>
2022-11-10 15:59:42 +0200

    Fix path for RedactorView

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -13,8 +13,8 @@ urlpatterns = [
     path("topic/", TopicListView.as_view(), name="topic-list"),
     path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
     path("newspaper/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
-    path("newspaper/", RedactorListView.as_view(), name="redactor-list"),
-    path("newspaper/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactor/", RedactorListView.as_view(), name="redactor-list"),
+    path("redactor/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
 ]
 
 app_name = "newsapp"
