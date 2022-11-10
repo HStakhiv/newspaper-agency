947b48e5667fb4c215f109803cacf5bccff99dc7
HStakhiv <gsstahiv@gmail.com>
2022-11-10 17:57:44 +0200

    Add accounts path

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('newsapp.urls', namespace="newsapp")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
