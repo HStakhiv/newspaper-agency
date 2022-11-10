a4c8bba8460d3359993cc553c0f4efaac6fc81e0
HStakhiv <gsstahiv@gmail.com>
2022-11-10 15:36:31 +0200

    Update urls.py (add path for new views)

--- newsapp/urls.py
+++ newsapp/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
-from newsapp.views import index
+from newsapp.views import (
+    index,
+    TopicListView,
+    NewspaperListView,
+    NewspaperDetailView,
+    RedactorListView,
+    RedactorDetailView,
+)
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("topic/", TopicListView.as_view(), name="topic-list"),
+    path("newspaper/", NewspaperListView.as_view(), name="newspaper-list"),
+    path("newspaper/<int:pk>/", NewspaperDetailView.as_view(), name="newspaper-detail"),
+    path("newspaper/", RedactorListView.as_view(), name="redactor-list"),
+    path("newspaper/<int:pk>/", RedactorDetailView.as_view(), name="redactor-detail"),
 ]
 
 app_name = "newsapp"
