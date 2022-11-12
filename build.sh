410a93575c2407b960ca0e1da4d49e49b1fcd1dd
HStakhiv <gsstahiv@gmail.com>
2022-11-12 19:52:51 +0200

    Change requirements.txt

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r ./requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
